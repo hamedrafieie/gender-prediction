a424d55a735e587fc190348b04177d5ede443374
Hamed Rafiei <hamed.rafie78@gmail.com>
2024-05-08 16:40:34 +0330

    random name done and unnecessary files deleted

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
-from logic import get_gender_and_country
-from matplotlib.pyplot import text
 import streamlit as st
+from logic import get_gender_and_country
+from faker import Faker
 
+st.title(':earth_americas::man_and_woman_holding_hands: Gender and Country Prediction')
 
-st.title(':earth_americas::man_and_woman_holding_hands: Gender and county prediction')
+# Create a Faker instance for generating random names
+fake = Faker(['fa_IR', 'en_US',"fr_FR", "it_IT", "de_DE", "ar_AE"])
 
-option = st.toggle("Random")
+# Add a "Random" button
+if st.button("Random"):
+    random_name = fake.first_name()
+    text_input = st.text_input("Enter a name 👇", value=random_name)
+else:
+    text_input = st.text_input("Enter a name 👇")
 
-text_input = st.text_input("Enter a name 👇")
 predicted_gender, predicted_country = get_gender_and_country(text_input)
-
-st.write(f"Predicted gender: ```{predicted_gender}```")
-st.write(f"Predicted country: ```{predicted_country}```")
+st.write(f"Predicted gender: {predicted_gender}")
+st.write(f"Predicted country: {predicted_country}")
